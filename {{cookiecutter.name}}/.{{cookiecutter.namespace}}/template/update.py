9a0681fd8d398bd834cb10fd04197f414106407c
Shengyu Zhang <i@silverrainz.me>
2023-02-08 23:17:32 +0800

    update

---
+++ {{cookiecutter.name}}/.{{cookiecutter.namespace}}/template/update.py
@@ -0,0 +1,6 @@
+#!/user/bin/env python3
+
+from cookiecutter.main import cookiecutter
+
+# Create project from the cookiecutter-pypackage.git repo template
+cookiecutter('gh:{{ cookiecutter.github_owner }}/template')

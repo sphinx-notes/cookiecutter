b8220afebbc6716e493250f8203466974438b15e
Shengyu Zhang <i@silverrainz.me>
2023-02-08 14:00:48 +0800

    update

--- {{cookiecutter.name}}/.{{cookiecutter.namespace}}/template/update.sh
+++ {{cookiecutter.name}}/.{{cookiecutter.namespace}}/template/update.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+cookiecutter=cookiecutter
+
 repo=$(basename --suffix .git `git config --get remote.origin.url`)
 root=$(git rev-parse --show-toplevel)
 
-if [[ "$PWD" != "$root" ]]; then
-    echo PWD must be at the root of the repository
+if [[ "$(basename $root)" != "$repo" ]]; then
+    echo repository root directory must be consistent with the repository name:
+    echo
+    echo \t"$(basename $root)" != "$repo"
     exit 1
 fi
 
-git submodule update --init
+if [[ "$PWD" != "$root" ]]; then
+    echo PWD must be root directory of the repository:
+    echo
+    echo \t"$PWD" != "$root"
+    exit 1
+fi
+
+# git submodule update --init
 
 # Generate cookiecutter.json for current project.
-cd "$root/.{{ cookiecutter.namespace }}/template"
-./gen_cookiecutter_json.py "$repo"
+"$root/.{{ cookiecutter.namespace }}/template/template/gen_cookiecutter_json.py" "$repo"
 
-# Generate project.
 cd "$root/.."
-cookiecutter ./template --no-input --overwrite-if-exists
+# Generate project.
+$cookiecutter ./template --no-input --overwrite-if-exists

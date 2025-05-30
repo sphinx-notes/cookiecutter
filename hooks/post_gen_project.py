13c3a3ce32995f5a6733756c8b3eb4bebb2848bb
Shengyu Zhang <i@silverrainz.me>
2025-05-31 03:00:27 +0800

    feat: Add "is_python_project" and "is_sphinx_extension" (#14)

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+import os
+
+REMOVE_PATHS = [
+    # {% if not cookiecutter.is_python_project %}
+    'pyproject.toml',
+    'ruff.toml',
+    'MANIFEST.in',
+    'src/{{ cookiecutter.namespace }}/{{ cookiecutter.name }}/.gitkeep',
+    # {% endif %}'
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        os.unlink(path) if os.path.isfile(path) else os.rmdir(path)

7cadd39d13e7e95f37aecb9e998f4d7bb66dbf10
Shengyu Zhang <i@silverrainz.me>
2025-09-13 10:54:18 +0800

    ruff: Format meta.py

--- {{cookiecutter.name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.name}}/meta.py
+++ {{cookiecutter.name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.name}}/meta.py
@@ -17,18 +17,20 @@ try:
 except metadata.PackageNotFoundError:
     __version__ = 'unknown'
 
-#{% if cookiecutter.is_sphinx_extension %}
+{% if cookiecutter.is_sphinx_extension %}
 ################################################################################
 # Sphinx extension utils.
 ################################################################################
 
+
 def pre_setup(app):
     app.require_sphinx('{{ cookiecutter.sphinx_version }}')
+
 
 def post_setup(app):
     return {
         'version': __version__,
         'parallel_read_safe': True,
-        'parallel_write_safe': True
+        'parallel_write_safe': True,
     }
-#{% endif %}
+{%- endif %}
